f1c328f7aea61f7223335e2c177752c33999ced2
xschen <xschen@trip.com>
2022-01-06 12:36:00 +0800

    11 路由

---
+++ ch11/vue-router-demo/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+</script>
+
+<template>
+  <div id="app">
+    <router-link to="/">home</router-link> | 
+    <router-link to="/about">about</router-link>
+  </div>
+
+<router-view></router-view>
+  
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
